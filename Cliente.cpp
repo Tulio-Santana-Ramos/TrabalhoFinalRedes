a92781b32304b99b491ed88cd90cabef3d3d3aff
Israel Silva <israelfelipe9@gmail.com>
2022-06-07 01:23:18 -0300

    Add files via upload

---
+++ Cliente.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <bits/stdc++.h>
+
+#define LIMITE_MENSAGEM 11
+
+using namespace std;
+
+int main(){
+	int fd_servidor;
+	sockaddr_in endereco_servidor;
+
+	char mensagem[LIMITE_MENSAGEM];
+	memset(mensagem, 0, LIMITE_MENSAGEM * sizeof(char));
+
+	//criação do socket do servidor
+	fd_servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Verificação se o socket foi devidamente criado:
+	if(fd_servidor == -1){
+		cerr << "Erro ao criar o socket!\n";
+		exit(-1);
+	}
+	cout << "Socket criado com sucesso!\n";
+
+	// Seta a porta e o IP para o mesmo endereço do servidor
+	endereco_servidor.sin_family = AF_INET;
+	endereco_servidor.sin_port = htons(2000);
+	endereco_servidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	// Verificação de conexão com o servidor
+	if(connect(fd_servidor, (sockaddr *) &endereco_servidor, sizeof(endereco_servidor)) < 0){
+		cerr << "Erro ao conectar com o servidor!\n";
+		exit(-1);
+	}
+	cout << "Conexão com servidor efetuada com sucesso\n";
+	
+	// Lê a mensagem do usuário
+	string total;
+	cout << "Digite uma mensagem: \n";
+	getline(cin, total);
+
+	int j = 0;
+	for(int i = 0; i < total.size(); i++){
+		mensagem[j++] = total[i];
+		if(i == total.size() - 1 || j == LIMITE_MENSAGEM - 1){
+			mensagem[j] = '\0';
+			// Envia a mensagem ao servidor
+			if(send(fd_servidor, mensagem, strlen(mensagem), 0) < 0){
+				cout << "Erro ao enviar a mensagem!\n";
+				exit(-1);
+			}	
+			j = 0;
+		}
+	}
+
+	// Sinaliza final do envio
+	mensagem[0] = '\0';
+
+	if(send(fd_servidor, mensagem, strlen(mensagem), 0) < 0){
+		cout << "Erro ao enviar a mensagem!\n";
+		exit(-1);
+	}
+
+	// Tratar aqui se receber mensagem de resposta do servidor
+
+	close(fd_servidor);
+
+	return 0;
+}
---
+++ Servidor.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <bits/stdc++.h>
+
+#define LIMITE_MENSAGEM 11
+
+using namespace std;
+
+
+int main(void) {
+
+    int fd_servidor, fd_cliente;  //? fd_cliente será um array?
+    sockaddr_in endereco_servidor, endereco_cliente;
+    socklen_t tamanho_endereco;
+
+    // Criação de buffer para as mensagens:
+    char mensagem[LIMITE_MENSAGEM];
+
+    // Criação do socket do servidor:
+    fd_servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Verificação se o socket foi devidamente criado:
+    if (fd_servidor == -1) {
+        cerr << "Erro ao criar o socket!\n";
+        exit(-1);
+    }
+    cout << "Socket criado com sucesso!\n";
+
+    // Configuração do endereço do servidor:
+    endereco_servidor.sin_family = AF_INET;
+    endereco_servidor.sin_port = htons(2000);
+    endereco_servidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Atribuição do endereço ao socket:
+    if (bind(fd_servidor, (sockaddr *) &endereco_servidor, sizeof(endereco_servidor)) == -1) {
+        cerr << "Erro ao realizar bind do socket!\n";
+        exit(-1);
+    } else {
+        cout << "Bind realizado com sucesso!\n";
+    }
+
+    // Marcação do socket para a escuta de um único cliente:
+    if (listen(fd_servidor, 1) == -1) {
+        cerr << "Erro na marcação de escuta por clientes!\n";
+        exit(-1);
+    } else {
+        cout << "Servidor escutando por requisições!\n";
+    }
+
+    // Aceite de uma requisição:
+    tamanho_endereco = sizeof(endereco_cliente);
+    fd_cliente = accept(fd_servidor, (sockaddr*) &endereco_cliente, &tamanho_endereco);
+
+    // Verificação se o aceite foi válido:
+    if (fd_cliente < 0){
+        cerr << "Não foi possível se conectar ao cliente!\n";
+        exit(-1);
+    } 
+    cout << "Cliente conectado no IP: " << inet_ntoa(endereco_cliente.sin_addr) << " e porta " << ntohs(endereco_cliente.sin_port) << endl;
+
+    while(true){
+        bzero(mensagem, LIMITE_MENSAGEM);
+
+        // Recebimento da mensagem do cliente:
+        if (recv(fd_cliente, mensagem, sizeof(mensagem) - 1, 0) == -1) {
+            cerr << "Mensagem não recebida!\n";
+            exit(-1);
+        }
+        
+        if(strlen(mensagem) == 0)
+            break;
+
+        printf("%s\n", mensagem);
+        
+        bzero(mensagem, LIMITE_MENSAGEM);
+    }
+
+    // Fechamento do socket:
+    close(fd_servidor);
+
+    return 0;
+}
